2cfd63bf7e7f627d60cef1693be485ae61df4840
eheperson <barannkuzu@gmail.com>
2022-03-14 15:54:42 +0300

    npm info file

---
+++ nodejs/npm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# This bash script file to note the basic npm(node package manager) commands
+
+echo 'npm : global command'
+echo 'npm --version : prints npm version'
+
+echo ' '
+echo ' '
+
+echo 'local dependencies'
+echo 'npm i <package_name>'
+
+echo ' '
+echo ' '
+
+echo 'global dependencies : '
+echo 'npm -i -g <package_name>'
+echo 'npm -i --global <package_name>'
+
+echo ' '
+echo ' '
+
+echo 'package.json : manifest file(stores important info about project/package)'
+echo 'manual approach : create package.json in the root'
+echo 'npm init : initialize npm project'
+echo 'npm init -y : initialize npm project with everything default'
