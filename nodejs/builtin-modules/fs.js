a855c5ffe244289c74fe2fbdd4168cbf5fbb48d3
eheperson <barannkuzu@gmail.com>
2022-03-14 15:54:27 +0300

    builtin modules seperated to a folder

---
+++ nodejs/builtin-modules/fs.js
@@ -0,0 +1,57 @@
+
+// -- -- - --- - - -- - fs Module
+// fs sync
+const { readFileSync, writeFileSync } = require('fs')
+console.log('start')
+const first = readFileSync('./content/first.txt', 'utf8')
+const second = readFileSync('./content/second.txt', 'utf8')
+
+writeFileSync(
+  './content/result-sync.txt',
+  `Here is the result : ${first}, ${second}`,
+  { flag: 'a' }
+)
+console.log('done with this task')
+console.log('starting the next one')
+
+// fs async
+const { readFile, writeFile } = require('fs')
+
+console.log('start')
+
+readFile('./content/first.txt', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(result)
+})
+
+readFile('./content/first.txt', 'utf8', (err, result) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    const first = result
+    readFile('./content/second.txt', 'utf8', (err, result) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const second = result
+        writeFile(
+            './content/result-async.txt',
+            `Here is the result : ${first}, ${second}`,
+            (err, result) => {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                console.log('done with this task')
+            }
+        )
+    })
+})
+
+console.log('starting next task')
+
---
+++ nodejs/builtin-modules/os.js
@@ -0,0 +1,19 @@
+// -- -- - --- - - -- - os Module
+// os module allows us to interact with os
+const os = require('os')
+
+// info about current user
+const user = os.userInfo()
+console.log(user)
+
+// method returns the system uptime in seconds
+console.log(`The System Uptime is ${os.uptime()} seconds`)
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMem: os.totalmem(),
+  freeMem: os.freemem(),
+}
+console.log(currentOS)
+
