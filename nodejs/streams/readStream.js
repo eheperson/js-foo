75e8d5aa61c3e25d193e87354bbcea5c6795f427
eheperson <barannkuzu@gmail.com>
2022-03-16 17:27:45 +0300

    stream info included

--- nodejs/streams/readStream.js
+++ nodejs/streams/readStream.js
@@ -1,12 +1,25 @@
 const fs = require('fs')
 
 var readStream = fs.createReadStream('readMe.txt', 'utf8');
+/*
+default size of the buffer : 64kb
+last buffer                : remainder
+highWaterMark              : this parameters controls the size of the buffer
+    
+
+    var readStream = fs.createReadStream('readMe.txt', {encoding:'utf8'});
+    var readStream = fs.createReadStream('readMe.txt', {highWateMark:9000});
+
+*/
 
 readStream.on('data', chunk=>{
     console.log('new chunk received ..');
     console.log(chunk);
 });
 
+readStream.on('error', err=> {
+    console.log(err);
+});
 readStream.on('end', ()=>{
     console.log("chunk end reached ...")
 })
---
+++ nodejs/streams/streams.js
@@ -0,0 +1,14 @@
+/*
+
+
+    Streams are a way to handle reading/writing files, network communications, 
+    or any kind of end-to-end information exchange in an efficient way
+
+    There are 4 types of streams in node:
+
+        1- Writable
+        2- Readable
+        3- Duplex
+        4- Transform
+
+*/
